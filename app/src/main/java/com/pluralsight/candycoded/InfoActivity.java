08b2b8892d7894b6f9d1c41e3192b6ce984da944
rm <recussivef@gmail.com>
2019-11-01 22:01:19 +0300

    bug: correct uri

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -30,7 +30,7 @@ public class InfoActivity extends AppCompatActivity {
 
     // ***
     public void createMapIntent(View view){
-        Uri uriAdress  = Uri.parse("geo:0,0?q=618 E South St Orlando,Fl 32001");
+        Uri uriAdress  = Uri.parse("geo:0,0?q=618 E South St Orlando,Fl 32801");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, uriAdress);
 
         mapIntent.setPackage("com.google.android.apps.maps");
